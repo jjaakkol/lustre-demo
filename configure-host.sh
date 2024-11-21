d00833433b069b4103cedfdb8140cb7aa34ad088
Jani Jaakkola <jani.jaakkola@helsinki.fi>
2024-11-21 02:38:27 -0500

    configure ssh known hosts

--- configure-host.sh
+++ configure-host.sh
@@ -5,5 +5,6 @@ host=lustre-demo-$1
 echo "Configuring this host as $host"
 hostnamectl set-hostname $host
 cp -v etc-issue /etc/issue
+cp -va known_hosts /etc/ssh/ssh_known_hosts
 rm /root/.bash_history
 
