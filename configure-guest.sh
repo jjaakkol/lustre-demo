b781d8d7aa2ffb70dbb1547c94c2a3ac1b025a81
Jani Jaakkola <jjaakkol@cs.helsinki.fi>
2024-11-21 11:10:37 +0200

    No need to remove .bash_history really

--- configure-guest.sh
+++ configure-guest.sh
@@ -11,5 +11,5 @@ echo "Configuring guest $(uname -r) as $guest"
 hostnamectl set-hostname $guest
 cp -v etc-issue /etc/issue
 cp -va known_hosts /etc/ssh/ssh_known_hosts
-rm /root/.bash_history
+#rm -f /root/.bash_history
 
