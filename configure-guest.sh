efc0d56e465752a775957046ca459fe49d96b809
Jani Jaakkola <jjaakkol@cs.helsinki.fi>
2024-11-26 20:05:43 +0200

    Be more quiet

--- configure-guest.sh
+++ configure-guest.sh
@@ -7,12 +7,11 @@ if [ -z "$guest" ]; then
   exit 1
 fi
 
-echo "Configuring guest $(uname -r) as $guest"
 hostnamectl set-hostname $guest
-cp -v etc-issue /etc/issue
+cp etc-issue /etc/issue
 cp -va known_hosts /etc/ssh/ssh_known_hosts
 rm /etc/machine-id
 systemd-machine-id-setup
-genhostid
+genhostid 2>/dev/null
 #rm -f /root/.bash_history
-
+echo "Configured guest $(uname -r) as $guest"
